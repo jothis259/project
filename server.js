d67ea94ed4d864cd46b46ac89e156090f027ce81
jothis259 <133837215+jothis259@users.noreply.github.com>
2024-03-03 12:03:47 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app=express();
+const port=8080;
+
+app.get('/',(req,res)=>{
+    res.send("my first project")
+})
+
+app.listen(port, ()=>{
+    console.log('Server Start , Port', port)
+})
